f4dfe6f176b4675a00e1aa7c86a6065bb1b13917
Erik <erik.steinman@hotmail.com>
2018-09-21 10:32:51 +0200

    working functionality

--- rps.py
+++ rps.py
@@ -1,8 +1,18 @@
 import random
 
-player = input ("Please choose either rock/paper/scissor: ")
-player = player.lower()
-while (player != "rock" and player != "paper" and player != "scissor"):
-    print(player);
-    player = input("That choice is not valid. Enter your choice (rock/paper/scissor): ");
-    player = player.lower();
+moves = ['rock', 'paper', 'scissors']
+player_wins = ['rockscissors', 'scissorspaper', 'paperrock']
+
+player_move = input ("Please choose either rock/paper/scissor: ")
+computer_move = random.choice(moves)
+
+
+print ("Your move was: ", player_move)
+print ("Computer choosed", computer_move)
+
+if player_move == computer_move:
+    print("It's a tie")
+elif player_move + computer_move in player_wins:
+    print("You win!")
+else:
+    print("You lose..."")
